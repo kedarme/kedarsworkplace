8de84fb7ea392a0784b4eab3aca10b6a3f12d206
kedar.me <kedar.me@8ac12dc1-7c3f-0410-9070-89a53124ee38>
2008-08-08 15:39:26 +0000

    first India commit

--- Uav_project/friedland_1.m
+++ Uav_project/friedland_1.m
@@ -4,14 +4,14 @@
 clear all;
 
 % Declaring all constants
-n = 6;       % Number of laps
+n = 10;       % Number of laps
 g = 9.8;      % Gravity constant
 r = 35;       % Radius
 h = 10;       % Height
 toutput = 1;  % Time period for availability of Output Sample
 estrate = 10; % Number of estimates before Output is available
 w = 1/25;     % Angular velocity
-snr = 75;     % Signal to noise ratio
+snr = 65;     % Signal to noise ratio
 % Accelorometer Gains
 Kax=1;
 Kay=1;
@@ -37,7 +37,18 @@ Ya = zeros(6,1); % Actual Output.
 res = zeros(6,1); % residual => Ya-Yc
 A = zeros(9,9);
 C = zeros(9,6);   % Corresponds to H in text
-P = zeros(9);
+%P = zeros(9);
+P = [ 0,0,0,0,0,0,0,0,0;
+    0,0,0,0,0,0,0,0,0;
+    0,0,0,0,0,0,0,0,0;
+    0,0,0,0,0,0,0,0,0;
+    0,0,0,0,0,0,0,0,0;
+    0,0,0,0,0,0,0,0,0;
+    0,0,0,0,0,0,0,0,0;
+    0,0,0,0,0,0,0,0,0;
+    0,0,0,0,0,0,0,0,0;
+    ];
+
 T = zeros(9); % Async to Pnext
 Q = eye(9);
 R = eye(6);
@@ -55,8 +66,8 @@ C_bias = zeros(6,6); % Corresponds to C in text
 S_bias = zeros(6,6); % Corresponds to 6 outputs X 6 bias parameters eqn 63
 
 B_bias = zeros(9,6); % Corresponds to B in text
-M_bias = zeros(6,6); % Dimensions from eqn 16
-
+%M_bias = eye(6,6); % Dimensions from eqn 16 initial condn from 23 equated to Pb????
+M_bias = zeros(6,6);
 b_bias = zeros(6,1); % Bias vector in terms of absolute value
 delta = zeros(9,1);
 
@@ -73,29 +84,28 @@ Yz = 0;
 % Generate values for sensor inputs
 % Gyro has bais of the magnitudee 20/3600/180*pi ==   9.6963e-005
 U = [0+0.00005;0+0.00005;w+0.00005];
+% U = [0+0.05;0+0.05;w+0.05];
 % Adding Noise to sensor inputs
-U = awgn(U(:,1),snr);
+U = awgn(U(:,1),snr); % Adds white gaussian noise with power  1/snr times the signal power for snr = 75 ; Pnoise = 0.013 * Psignal or 1.33% of Psignal
 
 
 for i = 1:ns-1 % Start the loop
 
     % System states for this iteration
-    x = X(1,i);
-    y = X(2,i);
-    z = X(3,i);    %3
-    Vx = X(4,i);
-    Vy = X(5,i);
-    Vz = X(6,i);   %6
-    psi = X(7,i);
-    theta = X(8,i);
-    phi = X(9,i);  %9
+    x = X(1,i);      % Linear Position Unit m
+    y = X(2,i);      % Linear Position Unit m
+    z = X(3,i);      % Linear Position Unit m
+    Vx = X(4,i);     % Linear Velocity Unit m/s
+    Vy = X(5,i);     % Linear Velocity Unit m/s
+    Vz = X(6,i);     % Linear Velocity Unit m/s
+    psi = X(7,i);    % Angular Position Unit rad
+    theta = X(8,i);  % Angular Position Unit rad
+    phi = X(9,i);    % Angular Position Unit rad
 
-    % Input Values for Calculations
-    wpsi = U(1,1);
-    wtheta = U(2,1);
-    wphi = U(3,1);
-
-
+    % Input Values for Calculations. Gyro Readings Angular Velocity
+    wpsi = U(1,1);     % Unit rad/s
+    wtheta = U(2,1);   % Unit rad/s
+    wphi = U(3,1);     % Unit rad/s
 
     C = [1,0,0,0,0,0,0,0,0;
         0,1,0,0,0,0,0,0,0;
@@ -132,7 +142,7 @@ for i = 1:ns-1 % Start the loop
             0,0,0,0,0,0, -sin(psi)*(Kgtheta*wtheta)-cos(psi)*(Kgphi*wphi),0,0;
             0,0,0,0,0,0, cos(psi)/cos(theta)*(Kgtheta*wtheta)-sin(psi)/cos(theta)*(Kgphi*wphi),sin(psi)/cos(theta)^2*(Kgtheta*wtheta)*sin(theta)+cos(psi)/cos(theta)^2*(Kgphi*wphi)*sin(theta),0;
             ];
-
+        % B_bias => B 
         B_bias = [
             0,0,0,0,0,0;
             0,0,0,0,0,0;
@@ -159,9 +169,14 @@ for i = 1:ns-1 % Start the loop
         act_Bax = 0;
         act_Bay = 0;
         act_Baz = 0;
+
+        % Creating the Outputs
+        % Accelerometer Readings - Linear Acceleration - Unit m/s^2
         Ax = ( (-act_Vy*(act_wphi)/g + act_Vz*(act_wtheta))/g + sin(act_theta) )*Kax + act_Bax;
         Ay = ( (act_Vx*(act_wphi)/g - act_Vz*(act_wpsi))/g - sin(act_psi)*cos(act_theta))*Kay + act_Bay;
         Az = ( (-act_Vx*(act_wtheta)/g + act_Vy*(act_wpsi))/g - cos(act_theta)*cos(act_psi))*Kaz + act_Baz;
+
+        % GPS Readings - Gives x,y,z cordinates(Absolute Values) - Unit m
         Yx = r*cos(w*t(1,i));
         Yy = r*sin(w*t(1,i));
         Yz = h;
@@ -180,6 +195,10 @@ for i = 1:ns-1 % Start the loop
         % Bias Calculation
         S_bias = ( C * U_x_bias ) + C_bias; % eqn 63
         V_x_bias = U_x_bias - (K * S_bias);  % eqn 62
+        Spec_A = M_bias;
+        Spec_B = (M_bias * S_bias');
+        Spec_C = inv( ( C * P * C') + R + (S_bias * M_bias * S_bias') );
+        Spec_D = (S_bias * M_bias);
         M_bias = M_bias - ( (M_bias * S_bias')* inv( ( C * P * C') + R + (S_bias * M_bias * S_bias') )  * (S_bias * M_bias) );
         K_b = M_bias * ( ( V_x_bias' * C') + C_bias' ) * inv(R);
 
@@ -196,10 +215,17 @@ for i = 1:ns-1 % Start the loop
     if (i ~= 1)
         b_bias(:,i) = ( (eye(6) - ( K_b * S_bias )) * b_bias(:,i-1)) + (K_b*res);
     end  % State has been initialsed to zero. b_bias(:,i-1) will access OOR
+    Bax = b_bias(1,i);
+    Bay = b_bias(2,i);
+    Baz = b_bias(3,i);
+    Bgpsi = b_bias(4,i);
+    Bgtheta = b_bias(5,i);
+    Bgphi = b_bias(6,i);
+
     delta = V_x_bias * b_bias(:,i); % eqn pg 365 diagram
 
     % Adding bias to state. Look at fig 2 on pg 365
-    X(:,i)  = X(:,i) + delta;
+    %X(:,i)  = X(:,i) + delta;
 
 
     % For next loop
@@ -215,8 +241,19 @@ for i = 1:ns-1 % Start the loop
         cos(psi)*(Kgtheta*wtheta)+sin(psi)*(Kgphi*wphi);
         sin(psi)/cos(theta)*(Kgtheta*wtheta)+cos(psi)/cos(theta)*(Kgphi*wphi);
         ];
+
+    %     func = [ cos(theta)*cos(phi)*Vx+(sin(psi)*sin(theta)*cos(phi)-cos(psi)*sin(phi))*Vy+(-cos(psi)*sin(theta)*cos(phi)+sin(psi)*sin(phi))*Vz;
+    %              cos(theta)*sin(phi)*Vx+(sin(psi)*sin(theta)*sin(phi)+cos(psi)*cos(phi))*Vy+(cos(psi)*sin(theta)*sin(phi)-sin(psi)*cos(phi))*Vz;
+    %              -sin(theta)*Vx+sin(psi)*cos(theta)*Vy+cos(psi)*cos(theta)*Vz;
+    %               0;
+    %               0;
+    %               0;
+    %              Kgpsi*wpsi+Bgpsi+sin(psi)*tan(theta)*(Kgtheta*wtheta+Bgtheta)+cos(psi)*tan(theta)*(Kgphi*wphi+Bgphi);
+    %              cos(psi)*(Kgtheta*wtheta+Bgtheta)+sin(psi)*(Kgphi*wphi+Bgphi);
+    %              sin(psi)/cos(theta)*(Kgtheta*wtheta+Bgtheta)+cos(psi)/cos(theta)*(Kgphi*wphi+Bgphi);
+    %              ];
     % Approximate next states of the system
-    %     X(:,i+1) = A * X(:,i);
+    % X(:,i+1) = A * X(:,i);
     X(:,i+1) = X(:,i) + ts * func;
 
     %Aposteri
@@ -239,3 +276,7 @@ figure('Position',[1,8, 0.80*scrsz(3), 0.80*scrsz(4)])
 subplot(2,2,1),plot(t,X(4,:),'r',t,X(5,:),'b',t,X(6,:),'g');grid on;title('Linear Velocities vs Time');legend('Vx','Vy','Vz',1);
 subplot(2,2,2),plot(t,X(7,:),'r',t,X(8,:),'b',t,X(9,:),'g');grid on;title('Pitch-Roll-Yaw vs Time');legend('\psi','\theta','\phi',1);
 subplot(2,2,3),plot(t,X(1,:),'r',t,X(2,:),'b',t,X(3,:),'g');grid on;title('Position in Inertial frame vs Time');legend('x','y','z',1);
+b_bias(:,i+1) = b_bias(:,i);
+figure('Position',[1,8, 0.80*scrsz(3), 0.80*scrsz(4)])
+subplot(1,2,1),plot(t,b_bias(1,:),t,b_bias(2,:),t,b_bias(3,:));grid on;title('Accelerometer Bias vs Time');legend('Bax','Bay','Baz',1);
+subplot(1,2,2),plot(t,b_bias(4,:),t,b_bias(5,:),t,b_bias(6,:));grid on;title('Gyro Bias vs Time');legend('Bg\psi','Bg\theta','Bg\phi',1);
