06d5a83a73216ce099c92e9d63bcf5cdb0ac782d
kedar.me <kedar.me@8ac12dc1-7c3f-0410-9070-89a53124ee38>
2008-12-25 04:35:53 +0000

    Added comments

--- trunk/kedarsworkplace/usrbincrash/src/app/Item.java
+++ trunk/kedarsworkplace/usrbincrash/src/app/Item.java
@@ -1,36 +1,71 @@
 package app;
 
-class Item { 
-	
+/**
+ * This class holds the data for the an individual item in the inventory.
+ * 
+ * @author Kedar Kulkarni
+ * 
+ */
+class Item {
+	/** The SKU */
 	private String sku;
+	/** Weight of one item unit */
 	private Integer weight;
+	/** Price of one item unit */
 	private Integer price;
-	public Item() {
-		super();
-	}
-	public Item(String sku, Integer weight, Integer price) {
-		super();
-		this.sku = sku;
-		this.weight = weight;
-		this.price = price;
-	}
+
+	/**
+	 * Method to get the SKU
+	 * 
+	 * @return
+	 */
 	public String getSku() {
 		return sku;
 	}
+
+	/**
+	 * Method to set the SKU
+	 * 
+	 * @param sku
+	 */
 	public void setSku(String sku) {
 		this.sku = sku;
 	}
+
+	/**
+	 * Method to get the weight
+	 * 
+	 * @return
+	 */
 	public Integer getWeight() {
 		return weight;
 	}
+
+	/**
+	 * Method to set the weight
+	 * 
+	 * @param weight
+	 */
 	public void setWeight(Integer weight) {
 		this.weight = weight;
 	}
+
+	/**
+	 * Method to get the Price
+	 * 
+	 * @return
+	 */
 	public Integer getPrice() {
 		return price;
 	}
+
+	/**
+	 * Method to set the Price
+	 * 
+	 * @param price
+	 */
 	public void setPrice(Integer price) {
 		this.price = price;
 	}
-	
+
 }
