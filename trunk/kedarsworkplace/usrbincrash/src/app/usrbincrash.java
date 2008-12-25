117bf1358551930b332a6571bbeec3c30fe053a8
kedar.me <kedar.me@8ac12dc1-7c3f-0410-9070-89a53124ee38>
2008-12-25 03:44:24 +0000

    Before removing record prints

--- trunk/kedarsworkplace/usrbincrash/src/app/usrbincrash.java
+++ trunk/kedarsworkplace/usrbincrash/src/app/usrbincrash.java
@@ -27,21 +27,24 @@ public class usrbincrash {
 		ds.scanFile(args[0]);
 		// Get how much weight needs to be dumped
 		weightToLoose = ds.getWeightToLoose();
-		// Get the inventory
+		// Get the inventory list
 		List<Item> itemList = ds.getItemList();
-
-		// System.out.println("The weightToLoose: " + weightToLoose);
+		// Sort by weight/cost
 		printList(itemList);
+		System.out.println("++++++++");
 		Collections.sort(itemList, new ItemComparator());
-		System.out.println("----------------------");
 		printList(itemList);
-		// System.out.println("----------------------");
+		// Get length of inventory
 		Integer noOfItems = itemList.size();
-
+		// List of records
 		List<Record> records = new ArrayList<Record>();
-		// Create a dummy record for first sweep. This will create the records for first item.
-		records.add(new Record(0,0,new Hashtable<String, Integer>()));
+		// Create a dummy record for first sweep. This will create the records
+		// for first item.
+		records.add(new Record(0, 0, new Hashtable<String, Integer>()));
+		// Temporary list of records for modification
 		List<Record> recordsTemp = new ArrayList<Record>();
+
+		// Loop through existing item list
 		for (int i = 0; i < noOfItems; i++) {
 			recordsTemp.clear();
 			Item tempItem = itemList.get(i);
@@ -50,57 +53,41 @@ public class usrbincrash {
 			records.clear();
 			records.addAll(recordsTemp);
 			recordsTemp.clear();
-
 			// Get all possible cases already considered.
 			Integer recordsSize = records.size();
-			System.out.println("Size=" + recordsSize + "::i=" + i);
-			//if (recordsSize != 0) {
-				for (int k = 0; k < recordsSize; k++) {
-					Record oldRecord = (Record) records.get(k);
-					Hashtable<String, Integer> oldCases = oldRecord.getCases();
-					Integer oldPrice = oldRecord.getSumPrice();
-					Integer oldWeight = oldRecord.getSumWeight();
-					Integer oldWeightConstant = oldWeight;
-					Integer x = 0;
-					while (oldWeight <= weightToLoose) {
-						x += 1;
-						oldWeight += tempItem.getWeight();
-					}
-
-					// Create List of records
-					for (Integer j = 0; j <= x; j++) {
-					//	System.out.println("--here--");
-						Hashtable<String, Integer> newCases = new Hashtable<String, Integer>(
-								oldCases);
-						newCases.put(tempItem.getSku(), j);
-						Integer sumPrice = oldPrice + (tempItem.getPrice() * j);
-						Integer sumWeight = oldWeightConstant + (tempItem.getWeight() * j);
-						Record tempRec = new Record(sumPrice, sumWeight, newCases);
-						recordsTemp.add(tempRec);
-						// printRecord(tempRec, "++++++++++++++++++++++++"+j);
-					}
-
-				}
-			/*} else {// First Record
-				Integer oldWeight = 0;
-				Integer x = 0;
+			// Loop through the older cases
+			for (int k = 0; k < recordsSize; k++) {
+				Record oldRecord = (Record) records.get(k);
+				Hashtable<String, Integer> oldCases = oldRecord.getCases();
+				Integer oldPrice = oldRecord.getSumPrice();
+				Integer oldWeight = oldRecord.getSumWeight();
+				Integer oldWeightConstant = oldWeight;
+				Integer x = 0; 
+				// Max number of current items to make the weight
 				while (oldWeight <= weightToLoose) {
-					System.out.println("------------------------------------------------there");
 					x += 1;
 					oldWeight += tempItem.getWeight();
 				}
-				// System.out.println("X:"+x);
+
+				// Create New Possible combinations leading to max number of current items
 				for (Integer j = 0; j <= x; j++) {
-					Hashtable<String, Integer> newCases = new Hashtable<String, Integer>();
+					// Start creating new case with existing older cases.
+					Hashtable<String, Integer> newCases = new Hashtable<String, Integer>(
+							oldCases);
+					// Append current item to list of cases
 					newCases.put(tempItem.getSku(), j);
-					Integer sumPrice = (tempItem.getPrice() * j);
-					Integer sumWeight = (tempItem.getWeight() * j);
+					// Update the Price of the record
+					Integer sumPrice = oldPrice + (tempItem.getPrice() * j);
+					// Update the Weight of the record
+					Integer sumWeight = oldWeightConstant
+							+ (tempItem.getWeight() * j);
+					// Create new record item
 					Record tempRec = new Record(sumPrice, sumWeight, newCases);
+					// Add record to list of records
 					recordsTemp.add(tempRec);
-					// printRecord(tempRec, "++++++++++++++++++++++++"+j);
 				}
+
 			}
-			*/
 			// Delete all old records
 			records.clear();
 			// Update with new records
@@ -108,7 +95,7 @@ public class usrbincrash {
 		}
 		System.out.println("Printing after loop");
 		// printListRecord(records);
-		//List<Record> recordsTemp = new ArrayList<Record>();
+		// List<Record> recordsTemp = new ArrayList<Record>();
 		recordsTemp.clear();
 		Integer initS = records.size();
 		System.out.println("Initial size=" + initS);
@@ -123,28 +110,42 @@ public class usrbincrash {
 		System.out.println("Stripped size=" + initF);
 
 		Collections.sort(recordsTemp, new RecordsComparator());
-		printRecord(recordsTemp.get(0),"+++++++++++++++++++");
-	//	printListRecord(recordsTemp);
+		// printRecord(recordsTemp.get(0), "+++++++++++++++++++");
+		printListRecord(recordsTemp);
 	}
 
 	private static List<Record> minimiser(List<Record> list) {
 		List<Record> newList = new ArrayList<Record>();
+		// Sort the current record stack by Price
+		Collections.sort(newList, new RecordsComparator());
 		Iterator<Record> it = list.iterator();
 		Record qualify = new Record(0, 0, null);
+		Integer minPrice = 0;
 		while (it.hasNext()) {
 			Record newRecord = (Record) it.next();
 			Integer recordWeight = newRecord.getSumWeight();
-			if (recordWeight >= weightToLoose) {
-				if (qualify.getSumPrice().equals(0)
-						&& qualify.getSumWeight().equals(0)) {
-					qualify.copy(newRecord); // first copy
-					newList.add(newRecord);
-				} else if (qualify.getSumPrice() < newRecord.getSumPrice()) {
-					newList.add(newRecord);
-				}
-			} else {
+			if (recordWeight < weightToLoose) { // can be recombined
 				newList.add(newRecord);
+			} else { // weight qualifies dump weight
+				Integer recordPrice = qualify.getSumPrice();
+				// Price has'nt been updated. This is first qualifier record.
+				// Set Price as min Price
+				if (minPrice == 0) {
+					minPrice = recordPrice;
+					// Add this record to newList
+					newList.add(newRecord);
+				} else if (recordPrice < minPrice) { // We have a record with
+														// price lower than our
+														// minPrice
+					minPrice = recordPrice;
+					// Add this record to newList
+					newList.add(newRecord);
+				} else { // We have a qualifier weight with price more than
+							// current minimum price
+					// Do nothing
+				}
 			}
+
 		}
 
 		return newList;
@@ -159,7 +160,7 @@ public class usrbincrash {
 					+ "]");
 		}
 	}
-	
+
 	private static void printListRecord(List<Record> list) {
 		Iterator<Record> it = list.iterator();
 		while (it.hasNext()) {
@@ -171,12 +172,14 @@ public class usrbincrash {
 	private static void printRecord(Record newRecord, String seperator) {
 		Hashtable<String, Integer> cases = newRecord.getCases();
 		System.out.println(seperator);
-		System.out.println("[Price=" + newRecord.getSumPrice() + "] [Weight=" + newRecord.getSumWeight() + "]");
+		System.out.println("[Price=" + newRecord.getSumPrice() + "] [Weight="
+				+ newRecord.getSumWeight() + "]");
 		System.out.println("{Case Values:");
 		Enumeration<String> e = cases.keys();
 		while (e.hasMoreElements()) {
 			String key = e.nextElement();
-			System.out.println("[Key=" + key + "] [Value=" + cases.get(key) + "]}");
+			System.out.println("[Key=" + key + "] [Value=" + cases.get(key)
+					+ "]}");
 		}
 	}
 
