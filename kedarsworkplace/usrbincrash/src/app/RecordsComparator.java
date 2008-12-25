b2659566e1628bf4410daf2756bd385ac8daaac3
kedar.me <kedar.me@8ac12dc1-7c3f-0410-9070-89a53124ee38>
2008-12-25 04:39:56 +0000

    Added comments

--- kedarsworkplace/usrbincrash/src/app/RecordsComparator.java
+++ kedarsworkplace/usrbincrash/src/app/RecordsComparator.java
@@ -2,10 +2,20 @@ package app;
 
 import java.util.Comparator;
 
+/**
+ * This class implements the Comparator for Record objects. We compare based on
+ * price.
+ * 
+ * @author Kedar Kulkarni
+ * 
+ */
 public class RecordsComparator implements Comparator<Record> {
-
+	/**
+	 * The overidden compare method.
+	 */
 	@Override
 	public int compare(Record obj1, Record obj2) {
+		// Return comparison between the prices.
 		return obj1.getSumPrice().compareTo(obj2.getSumPrice());
 	}
 
