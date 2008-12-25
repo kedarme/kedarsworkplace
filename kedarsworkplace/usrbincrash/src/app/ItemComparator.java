985b8be0957c355982c2c8492e057efd912859bd
kedar.me <kedar.me@8ac12dc1-7c3f-0410-9070-89a53124ee38>
2008-12-25 04:38:26 +0000

    Added comments

--- kedarsworkplace/usrbincrash/src/app/ItemComparator.java
+++ kedarsworkplace/usrbincrash/src/app/ItemComparator.java
@@ -2,12 +2,26 @@ package app;
 
 import java.util.Comparator;
 
+/**
+ * This class implements the Comparator for Item objects We compare based on
+ * price per unit weight.
+ * 
+ * @author Kedar Kulkarni
+ * 
+ */
 public class ItemComparator implements Comparator<Item> {
-
+	/**
+	 * The overidden compare method.
+	 */
 	@Override
 	public int compare(Item obj1, Item obj2) {
-		Double pricePerPound1 = (obj1.getPrice().doubleValue() / obj1.getWeight().doubleValue());
-		Double pricePerPound2 = (obj2.getPrice().doubleValue() / obj2.getWeight().doubleValue());
+		// Price per unit weight for obj1
+		Double pricePerPound1 = (obj1.getPrice().doubleValue() / obj1
+				.getWeight().doubleValue());
+		// Price per unit weight for obj1
+		Double pricePerPound2 = (obj2.getPrice().doubleValue() / obj2
+				.getWeight().doubleValue());
+		// Return comparison between the two
 		return pricePerPound1.compareTo(pricePerPound2);
 	}
 
